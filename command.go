0e64eafd842870f682e253625963d2ffc5b73d8f
Gerdriaan Mulder <mrngm@moeilijklastig.nl>
2026-09-03 16:01:28 +0200

    Properly disambiguate Bash `complete -A` and `complete -W`; small build-fix for fish (#25)
    * fix(Completer): also import fish
    
    * bash: disambiguate actions (complete/compgen -A) from regular completions
    
    ---------
    
    Co-authored-by: Gerdriaan Mulder <gmulder@science.ru.nl>

--- command.go
+++ command.go
@@ -26,7 +26,7 @@ type Completer interface {
 
 var (
 	_ Completer = (*Zsh)(nil)
-	_ Completer = (*Bash)(nil)
+	_ Completer = (*Fish)(nil)
 	_ Completer = (*Bash)(nil)
 )
 
@@ -161,7 +161,7 @@ func toAction(action, shell string) string {
 	case "zsh":
 		return zshActions[action]
 	case "bash":
-		return action
+		return "ACT{" + action + "}"
 	case "fish":
 		return ""
 	}
